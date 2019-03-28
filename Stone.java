11ac6ac2460f5df2740368e47c92f9859c6e853b
Brandon ALVES BARBOSA <brandon.alves.barbosa@gmail.com>
2019-03-28 14:04:46 +0100

    ok Stone + ok Color

--- Stone.java
+++ Stone.java
@@ -1,13 +1,48 @@
-public enum Stone {
-	WHITE,
-	BLACK;
+public class Stone {
+	private Color color;
+	private int x;
+	private int y;
 
-	@Override
-	public String toString() {
-		return this == WHITE ? "white" : "black";
+	public Stone(Color color, int x, int y) {
+		this.color = color;
+		this.x = x;
+		this.y = y;
 	}
 
-	public Stone other() {
-		return this == WHITE ? BLACK : WHITE;
+	@Override
+	public boolean equals(Object object) {
+		if (object == null)
+			return false;
+		if (this.getClass() != object.getClass())
+			return false;
+		Stone stone = (Stone) object;
+		if (this.color != stone.color)
+			return false;
+		if (this.x != stone.x)
+			return false;
+		if (this.y != stone.y)
+			return false;
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		return "{" +
+				this.color.toString() +
+				", " + 
+				this.x + 
+				", " + 
+				this.y + 
+				"}";
+	}
+
+	public Color getColor() {
+		return this.color;
+	}
+	public int getX() {
+		return this.x;
+	}
+	public int getY() {
+		return this.y;
 	}
 }
