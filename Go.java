9ec2e4c85c69d8f379ce2945b17da85140c9b910
Brandon ALVES BARBOSA <brandon.alves.barbosa@gmail.com>
2019-03-28 15:01:49 +0100

    bug Parameter

--- Go.java
+++ Go.java
@@ -1,68 +1,76 @@
 
 public class Go {
-	public Stone[][] goban;
-	public Color turn;
-	public Parameter parameter;
+	private Color[][] goban;
+	private Color turn;
+	private Parameter parameter; // Exception if size != 19 | 13 | 9 !!
 
 	public Go(Parameter parameter) {
-		this.goban = new Stone[parameter.getSize()][parameter.getSize()];
-		for (Stone son[] : this.goban) {
-			for (Stone cell : son)
+		this.goban = new Color[parameter.getSize()][parameter.getSize()];
+		for (Color son[] : this.goban) {
+			for (Color cell : son)
 				cell = null;
 		}
 		this.turn = Color.BLACK;
 	}
 
-	// public void turnOver() {
-	// 	this.turn = (this.turn == Color.WHITE) ? Color.BLACK : Color.WHITE;
-	// }
+	public void turnOver() {
+		this.turn = this.turn.other();
+	}
 
-	// public void setStone(int x, int y) {
-	// 	this.goban.grid[y][x] = new Stone(this.turn, x, y);
-	// }
+	public Color getStatus(int x, int y) {
+		if (this.goban[y][x] == null)
+			return null;
+		if (this.goban[y][x] == Color.WHITE)
+			return Color.WHITE;
+		return Color.BLACK;
+	}
 
-	// public int getLiberty(Stone stone) {
-	// 	int liberty = 0;
-	// 	int x = stone.x;
-	// 	int y = stone.y;
-	// 	if (x == 0) {
-	// 		if (y == 0) {
-	// 			liberty += this.goban.grid[y+1][x] == null ? 1 : 0;
-	// 			liberty += this.goban.grid[y][x+1] == null ? 1 : 0;
-	// 		}
-	// 		else if (y == this.goban.size - 1) {
-	// 			liberty += this.goban.grid[y-1][x] == null ? 1 : 0;
-	// 			liberty += this.goban.grid[y][x+1] == null ? 1 : 0;
-	// 		}
-	// 		else {
-	// 			liberty += this.goban.grid[y][x+1] == null ? 1 : 0;
-	// 			liberty += this.goban.grid[y-1][x] == null ? 1 : 0;
-	// 			liberty += this.goban.grid[y+1][x] == null ? 1 : 0;
-	// 		}
-	// 	}
-	// 	else if (x == this.size - 1) {
-	// 		if (y == 0) {
-	// 			liberty += this.goban[y][x-1] == null ? 1 : 0;
-	// 			liberty += this.goban[y+1][x] == null ? 1 : 0;
-	// 		}
-	// 		else if (y == this.size - 1) {
-	// 			liberty += this.goban[y][x-1] == null ? 1 : 0;
-	// 			liberty += this.goban[y-1][x] == null ? 1 : 0;
-	// 		}
-	// 		else {
-	// 			liberty += this.goban[y-1][x] == null ? 1 : 0;
-	// 			liberty += this.goban[y+1][x] == null ? 1 : 0;
-	// 			liberty += this.goban[y][x-1] == null ? 1 : 0;
-	// 		}
-	// 	}
-	// 	else {
-	// 		liberty += this.goban[y][x-1] == null ? 1 : 0;
-	// 		liberty += this.goban[x][x+1] == null ? 1 : 0;
-	// 		liberty += this.goban[y-1][x] == null ? 1 : 0;
-	// 		liberty += this.goban[y+1][x] == null ? 1 : 0;
-	// 	}
-	// 	return liberty;
-	// }
+	public void setStone(Stone stone) {
+		this.goban[stone.getY()][stone.getX()] = stone.getColor();
+	}
+
+	public int getLiberty(Stone stone) {
+		int liberty = 0;
+		int x = stone.getX();
+		int y = stone.getY();
+		if (x == 0) {
+			if (y == 0) {
+				liberty += this.goban[y+1][x] == null ? 1 : 0;
+				liberty += this.goban[y][x+1] == null ? 1 : 0;
+			}
+			else if (y == this.parameter.getSize() - 1) {
+				liberty += this.goban[y-1][x] == null ? 1 : 0;
+				liberty += this.goban[y][x+1] == null ? 1 : 0;
+			}
+			else {
+				liberty += this.goban[y][x+1] == null ? 1 : 0;
+				liberty += this.goban[y-1][x] == null ? 1 : 0;
+				liberty += this.goban[y+1][x] == null ? 1 : 0;
+			}
+		}
+		else if (x == this.parameter.getSize() - 1) {
+			if (y == 0) {
+				liberty += this.goban[y][x-1] == null ? 1 : 0;
+				liberty += this.goban[y+1][x] == null ? 1 : 0;
+			}
+			else if (y == this.parameter.getSize() - 1) {
+				liberty += this.goban[y][x-1] == null ? 1 : 0;
+				liberty += this.goban[y-1][x] == null ? 1 : 0;
+			}
+			else {
+				liberty += this.goban[y-1][x] == null ? 1 : 0;
+				liberty += this.goban[y+1][x] == null ? 1 : 0;
+				liberty += this.goban[y][x-1] == null ? 1 : 0;
+			}
+		}
+		else {
+			liberty += this.goban[y][x-1] == null ? 1 : 0;
+			liberty += this.goban[x][x+1] == null ? 1 : 0;
+			liberty += this.goban[y-1][x] == null ? 1 : 0;
+			liberty += this.goban[y+1][x] == null ? 1 : 0;
+		}
+		return liberty;
+	}
 
 	// public boolean groupExists(Coord coord, Stone stone) {
 	// 	int bool;							// a finir
@@ -71,4 +79,32 @@ public class Go {
 	// 	return bool;
 	// }
 
+	@Override
+	public String toString() {
+		String string = new String();
+		string = "goban : \n\t";
+		for (Color son[] : this.goban) {
+			for (Color cell : son) {
+				if (cell == null)
+					string += ". ";
+				else if (cell == Color.WHITE)
+					string += "0 ";
+				else if (cell == Color.BLACK)
+					string += "1 ";
+			}
+			string += "\n\t";
+		}
+		string += "\nturn : \n\t" + this.getTurn().toString() + "\n\nparameter : \n\t" /*+ this.parameter.toString()*/;
+		return string;
+	}
+
+	public Color[][] getGoban() {
+		return this.goban;
+	}
+	public Color getTurn() {
+		return this.turn;
+	}
+	public Parameter getParameter() {
+		return this.parameter;
+	}
 }
