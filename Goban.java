2cdee017374d4e7ffb03625eeab2a65835cac481
Sevla <dasilvaalvesb@gmail.com>
2019-04-05 18:10:16 +0200

    Goban paintComponent

--- Goban.java
+++ Goban.java
@@ -1,23 +1,37 @@
 import javax.swing.JPanel;
 import java.awt.Graphics;
-import java.awt.Color;
+// import java.awt.Color;
 
 class Goban extends JPanel {
-	private int size;
-	private int caseSize;
+	private Go go;
 
-	public Goban() {
-		this.size = 19;
-		this.caseSize = 20;
-		this.setBackground(Color.ORANGE);
+	public Goban(Go go) {
+		// this.setBackground(Color.ORANGE);
+		this.go = go;
 	}
 
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		for (int i = 0; i < this.size; i += 1) {
-			g.drawLine(i*this.caseSize, 0, i*this.caseSize, (this.size-1)*this.caseSize);
-			g.drawLine(0, i*this.caseSize, (this.size-1)*this.caseSize, i*this.caseSize);
-			g.fillOval(50, 50, 50, 50);
+		for (int i = 0; i < this.go.getParameter().getSize(); i += 1) {
+			g.drawLine(i*go.getParameter().getCaseSize(), 0, i*go.getParameter().getCaseSize(), (this.go.getParameter().getSize()-1)*go.getParameter().getCaseSize());
+			g.drawLine(0, i*go.getParameter().getCaseSize(), (this.go.getParameter().getSize()-1)*go.getParameter().getCaseSize(), i*go.getParameter().getCaseSize());
+		}
+
+		for (int i = 0; i < this.go.getParameter().getSize(); i += 1) {
+			for (int j = 0; j < this.go.getParameter().getSize(); j += 1) {
+				if (this.go.getGoban()[i][j] != null) {
+					if (this.go.getGoban()[i][j] == Color.BLACK) {
+						int y = i*go.getParameter().getCaseSize() - go.getParameter().getCaseSize()/2;
+						int x = j*go.getParameter().getCaseSize() - go.getParameter().getCaseSize()/2;
+						g.fillOval(x, y, go.getParameter().getCaseSize(), go.getParameter().getCaseSize());
+					}
+					if (this.go.getGoban()[i][j] == Color.WHITE) {
+						int y = i*go.getParameter().getCaseSize() - go.getParameter().getCaseSize()/2;
+						int x = j*go.getParameter().getCaseSize() - go.getParameter().getCaseSize()/2;
+						g.fillOval(x, y, go.getParameter().getCaseSize(), go.getParameter().getCaseSize());
+					}
+				}
+			}
 		}
 	}
 }
