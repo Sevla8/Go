28a426cbb7d00bf488bb9a29cb20adfd900fa15a
Sevla <brandon.da-silva-alves@etu.u-pec.fr>
2019-05-19 01:11:21 +0200

    FirstScreen + Menu

--- MyFrame.java
+++ MyFrame.java
@@ -2,16 +2,30 @@ import javax.swing.JFrame;
 
 public class MyFrame extends JFrame {
 
-	public MyFrame(Goban goban) {
+	public MyFrame() {
 		this.setSize(700, 700);
 		this.setLocation(5, 30);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setTitle("Go");
-		// this.setLayout(null);
+		// this.setLayout(null);		
+	}
 
-		goban.setOpaque(true);
-		
+	public MyFrame(Goban goban) {
+		this();
 		this.add(goban);
+		goban.setOpaque(true);
+	}
+
+	public MyFrame(Menu menu) {
+		this();
+		this.add(menu);
+		menu.setOpaque(true);
+	}
+
+	public MyFrame(FirstScreen firstScreen) {
+		this();
+		this.add(firstScreen);
+		firstScreen.setOpaque(true);
 	}
 
 	public void run() {
--- Parameter.java
+++ Parameter.java
@@ -2,13 +2,11 @@ public class Parameter {
 	private int size;
 	private int caseSize = 30;
 	private int marge = 60;
-	private String watch;
+	private Watch watch;
 
-	public Parameter(int size, String watch) throws WatchException, SizeException {
+	public Parameter(int size, Watch watch) throws SizeException {
 		if (size != 9 && size != 13 && size != 19)
 			throw new SizeException();
-		if (!watch.equals("none") && !watch.equals("absolute") && !watch.equals("byo-yomi"))
-			throw new WatchException();
 		else {
 			this.size = size;
 			this.watch = watch;
@@ -20,11 +18,14 @@ public class Parameter {
 		return "size : \n\t" + this.size + 
 				"\ncaseSize : \n\t" + this.caseSize +
 				"\nmarge : \n\t" + this.marge +
-				"\nwatch : \n\t" + this.watch;
+				"\nwatch : \n\t" + this.watch.toString();
 	}
 
 	public int getSize() {
 		return this.size;
+	}
+	public void setSize(int size) {
+		this.size = size;
 	}
 	public int getCaseSize() {
 		return this.caseSize;
@@ -32,7 +33,10 @@ public class Parameter {
 	public int getMarge() {
 		return this.marge;
 	}
-	public String getWatch() {
+	public Watch getWatch() {
 		return this.watch;
+	}
+	public void setWatch(Watch watch) {
+		this.watch = watch;
 	}
 }
