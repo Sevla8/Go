ab80b8ba3670b2f8a3114a2eacb2b2b844ab175f
Sevla <brandon.da-silva-alves@etu.u-pec.fr>
2019-05-19 22:07:40 +0200

    Menu Layout

--- Menu.java
+++ Menu.java
@@ -1,7 +1,9 @@
 import java.awt.Color;
+import javax.swing.Box;
 import java.awt.Graphics;
 import javax.swing.JPanel;
 import javax.swing.JButton; 
+import javax.swing.BoxLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -11,16 +13,34 @@ public class Menu extends JPanel {
 	private JButton quit;
 
 	public Menu(Control control) {
+		this.setBackground(Color.BLACK);
 		this.start = new JButton("Start");
 		this.option = new JButton("Options");
 		this.quit = new JButton("Quit");
-		this.add(this.start);
-		this.add(this.option);
-		this.add(this.quit);
+
+		Box box1 = Box.createHorizontalBox();
+		box1.add(this.start);
+
+		Box box2 = Box.createHorizontalBox();
+		box2.add(this.option);
+
+		Box box3 = Box.createHorizontalBox();
+		box3.add(this.quit);
+
+		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		this.add(Box.createVerticalGlue());
+		this.add(Box.createVerticalGlue());
+		this.add(box1);
+		this.add(Box.createVerticalGlue());
+		this.add(box2);
+		this.add(Box.createVerticalGlue());
+		this.add(box3);
+		this.add(Box.createVerticalGlue());
+		this.add(Box.createVerticalGlue());
+
 		this.start.addActionListener(control);
 		this.option.addActionListener(control);
 		this.quit.addActionListener(control);
-		this.setBackground(Color.BLACK);
 	}
 
 	public JButton getStart() {
