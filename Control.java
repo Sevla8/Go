08663bd4762b23605ac27ee24087c1ff05ce88bc
Sevla <brandon.da-silva-alves@etu.u-pec.fr>
2019-05-19 20:15:45 +0200

    links between screens

--- Control.java
+++ Control.java
@@ -1,73 +1,107 @@
+import java.awt.event.MouseEvent;
 import java.awt.event.ActionEvent;
+import java.awt.event.MouseListener;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseMotionListener;
 
-public class Control implements ActionListener {
-	private Parameter parameter;
+public class Control implements MouseMotionListener, MouseListener, ActionListener {
 	private MyFrame myFrame;
 	private Menu menu;
 	private Option option;
 	private Goban goban;
+	private Click click;
 
 	public Control() {
+		this.myFrame = new MyFrame();
+		this.menu = new Menu(this);
+		this.option = new Option(this);
+		Parameter parameter = null;
 		try {
-			this.parameter = new Parameter(19, Watch.NONE);
+			parameter = new Parameter(19, Watch.NONE);
 		}
 		catch (SizeException sizeException) {
 			sizeException.printStackTrace();
 		}
-		this.myFrame = new MyFrame();
-		this.myFrame.setVisible(true);
-
+		Go go = new Go(parameter);
+		this.goban = new Goban(this, go);
 		this.addMenu();
+		this.myFrame.setVisible(true);
 	}
 
 	public void addMenu() {
-		this.menu = new Menu(this);
 		this.myFrame.setContentPane(this.menu);
 		this.menu.setOpaque(true);
+		this.myFrame.validate();
 	}
 	public void addOption() {
-		this.option = new Option(this);
 		this.myFrame.setContentPane(this.option);
 		this.option.setOpaque(true);
+		this.myFrame.validate();
 	}
-	private void remove() {
-		this.myFrame.getContentPane().removeAll();
+	public void addGoban() {
+		this.myFrame.setContentPane(this.goban);
+		this.option.setOpaque(true);
+		this.myFrame.validate();
 	}
 
 	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() == this.menu.getQuit()) {
+		if (e.getSource() == this.menu.getQuit())
 			this.myFrame.dispose();
-		}
-
-		else if (e.getSource() == this.menu.getOption()) {
-			//this.remove();
+		else if (e.getSource() == this.menu.getOption())
 			this.addOption();
-			// this.menu.repaint();
-			// this.option.repaint();
-			this.myFrame.getContentPane().repaint();
-		}
+		else if (e.getSource() == this.menu.getStart())
+			this.addGoban();
 
 		else if (e.getSource() == this.option.getSave() &&
 			(this.option.getNoneWatch().isSelected() || this.option.getAbsoluteWatch().isSelected() || this.option.getByoYomiWatch().isSelected()) &&
 			(this.option.getSize9().isSelected() || this.option.getSize13().isSelected() || this.option.getSize19().isSelected())) {
 			
 			if (this.option.getSize9().isSelected())
-				this.parameter.setSize(9);
+				this.goban.getGo().getParameter().setSize(9);
 			else if (this.option.getSize13().isSelected())
-				this.parameter.setSize(13);
+				this.goban.getGo().getParameter().setSize(13);
 			else 
-				this.parameter.setSize(19);
+				this.goban.getGo().getParameter().setSize(19);
 			if (this.option.getNoneWatch().isSelected())
-				this.parameter.setWatch(Watch.NONE);
+				this.goban.getGo().getParameter().setWatch(Watch.NONE);
 			else if (this.option.getAbsoluteWatch().isSelected())
-				this.parameter.setWatch(Watch.ABSOLUTE);
+				this.goban.getGo().getParameter().setWatch(Watch.ABSOLUTE);
 			else 
-				this.parameter.setWatch(Watch.BYO_YOMI);
+				this.goban.getGo().getParameter().setWatch(Watch.BYO_YOMI);
 		}
 		
-		else if (e.getSource() == this.option.getBack()) {
-			
+		else if (e.getSource() == this.option.getBack())
+			this.addMenu();
+	}
+
+	public void mousePressed(MouseEvent e) {
+		if (e.getButton() == MouseEvent.BUTTON1)
+			this.click = Click.LEFT;
+		else if (e.getButton() == MouseEvent.BUTTON2)
+			this.click = Click.SCROLL;
+		else if (e.getButton() == MouseEvent.BUTTON3)
+			this.click = Click.RIGHT;
+	}
+
+	public void mouseClicked(MouseEvent e) {
+		if (this.click == Click.LEFT) {
+			int caseSize = this.goban.getGo().getParameter().getCaseSize();
+			int marge = this.goban.getGo().getParameter().getMarge();
+
+			int x = (e.getX()-marge+caseSize/2)/caseSize;
+			int y = (e.getY()-marge+caseSize/2)/caseSize;
+
+			if (this.goban.getGo().inGoban(x, y)) {
+				this.goban.getGo().getGoban()[y][x] = this.goban.getGo().getTurn() == Player.WHITE ? Player.WHITE : Player.BLACK;
+				this.goban.repaint();
+			}
 		}
 	}
+
+	public void mouseDragged(MouseEvent e) {}
+	public void mouseMoved(MouseEvent e) {}
+	public void mouseExited(MouseEvent e) {}
+	public void mouseEntered(MouseEvent e) {}
+	public void mouseReleased(MouseEvent e) {}
+
 }
--- Goban.java
+++ Goban.java
@@ -2,22 +2,16 @@ import java.awt.Color;
 import java.awt.Point;
 import java.awt.Graphics;
 import javax.swing.JPanel;
-import java.awt.event.MouseEvent;
-import java.awt.event.ActionEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseMotionListener;
 
-class Goban extends JPanel implements MouseMotionListener, MouseListener, ActionListener {
+class Goban extends JPanel {
 	private Go go;
-	private Click click;
 
-	public Goban(Go go) {
+	public Goban(Control control, Go go) {
 		this.setBackground(Color.ORANGE);
 		this.go = go;
 
-		this.addMouseListener(this);
-		this.addMouseMotionListener(this);
+		this.addMouseListener(control);
+		this.addMouseMotionListener(control);
 	}
 
 	public void paintComponent(Graphics g) {
@@ -48,39 +42,7 @@ class Goban extends JPanel implements MouseMotionListener, MouseListener, Action
 		}
 	}
 
-	public void mousePressed(MouseEvent e) {
-		if (e.getButton() == MouseEvent.BUTTON1)
-			this.click = Click.LEFT;
-		else if (e.getButton() == MouseEvent.BUTTON2)
-			this.click = Click.SCROLL;
-		else if (e.getButton() == MouseEvent.BUTTON3)
-			this.click = Click.RIGHT;
-	}
-	public void mouseClicked(MouseEvent e) {
-		if (this.click == Click.LEFT) {
-			int caseSize = this.go.getParameter().getCaseSize();
-			int marge = this.go.getParameter().getMarge();
-
-			int x = (e.getX()-marge+caseSize/2)/caseSize;
-			int y = (e.getY()-marge+caseSize/2)/caseSize;
-
-			if (this.go.inGoban(x, y)) {
-				this.go.getGoban()[y][x] = this.go.getTurn() == Player.WHITE ? Player.WHITE : Player.BLACK;
-				this.repaint();
-			}
-		}
-	}
-	public void actionPerformed(ActionEvent e) {}
-	public void mouseDragged(MouseEvent e) {}
-	public void mouseMoved(MouseEvent e) {}
-	public void mouseExited(MouseEvent e) {}
-	public void mouseEntered(MouseEvent e) {}
-	public void mouseReleased(MouseEvent e) {}
-
 	public Go getGo() {
 		return this.go;
-	}
-	public Click getClick() {
-		return this.click;
 	}
 }
