969b88437bb076dbe217586102eeefc44774ec7e
Brandon ALVES BARBOSA <brandon.alves.barbosa@gmail.com>
2019-05-20 13:53:28 +0200

    Goban Layout

--- Goban.java
+++ Goban.java
@@ -2,47 +2,37 @@ import java.awt.Color;
 import java.awt.Point;
 import java.awt.Graphics;
 import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 
 class Goban extends JPanel {
-	private Go go;
+	private Board board;
+	private JLabel watch;
 
 	public Goban(Control control, Go go) {
 		this.setBackground(Color.ORANGE);
-		this.go = go;
 
-		this.addMouseListener(control);
-		this.addMouseMotionListener(control);
+		this.board = new Board(control, go);
+		this.watch = new JLabel("time");
+
+		Box box1 = Box.createHorizontalBox();
+		box1.add(new JButton("Previous"));
+		box1.add(new JButton("Following"));
+
+		Box box2 = Box.createVerticalBox();
+		box2.add(box1, this.watch);
+
+		this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+		this.add(this.board);
+		this.add(box2);
 	}
 
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		int size = this.go.getParameter().getSize();
-		int caseSize = this.go.getParameter().getCaseSize();
-		int marge = this.go.getParameter().getMarge();
-
-		g.setColor(Color.GRAY);
-		for (int i = 0; i < size; i += 1) {
-			g.drawLine(i*caseSize+marge, 0+marge, i*caseSize+marge, (size-1)*caseSize+marge);
-			g.drawLine(0+marge, i*caseSize+marge, (size-1)*caseSize+marge, i*caseSize+marge);
-		}
-
-		for (int i = 0; i < size; i += 1) {
-			for (int j = 0; j < size; j += 1) {
-				if (this.go.getGoban()[i][j] != null) {
-					if (this.go.getGoban()[i][j] == Player.BLACK) {
-						g.setColor(Color.BLACK);
-						g.fillOval(j*caseSize-caseSize/2+marge, i*caseSize-caseSize/2+marge, caseSize, caseSize);
-					}
-					if (this.go.getGoban()[i][j] == Player.WHITE) {
-						g.setColor(Color.WHITE);
-						g.fillOval(j*caseSize-caseSize/2+marge, i*caseSize-caseSize/2+marge, caseSize, caseSize);
-					}
-				}
-			}
-		}
+	public Board getBoard() {
+		return this.board;
 	}
-
-	public Go getGo() {
-		return this.go;
+	public JLabel getWatch() {
+		return this.watch;
 	}
 }
