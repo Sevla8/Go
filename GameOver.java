d5091e7de145da5aa776f569bd2e22aa5d3ca9e0
Sevla <brandon.da-silva-alves@etu.u-pec.fr>
2019-06-05 15:45:09 +0200

    make winner

--- GameOver.java
+++ GameOver.java
@@ -7,7 +7,7 @@ import javax.swing.BoxLayout;
 public class GameOver extends JPanel {
 	private JButton menu;
 
-	public GameOver(Control control, Player player) {
+	public GameOver(Control control, Player player, double blackScore, double whiteScore) {
 		JLabel label = new JLabel("GAME OVER");
 		Box box1 = Box.createHorizontalBox();
 		box1.add(label);
@@ -16,10 +16,18 @@ public class GameOver extends JPanel {
 		Box box2 = Box.createHorizontalBox();
 		box2.add(winner);
 
+		JLabel blackScoreLabel = new JLabel("Black Player Score : "+blackScore);
+		Box box3 = Box.createHorizontalBox();
+		box3.add(blackScoreLabel);
+
+		JLabel whiteScoreLabel = new JLabel("White Player Score : "+whiteScore);
+		Box box4 = Box.createHorizontalBox();
+		box4.add(whiteScoreLabel);
+
 		this.menu = new JButton("Menu");
 		this.menu.addActionListener(control);
-		Box box3 = Box.createHorizontalBox();
-		box3.add(this.menu);
+		Box box5 = Box.createHorizontalBox();
+		box5.add(this.menu);
 
 		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		this.add(Box.createVerticalGlue());
@@ -29,6 +37,10 @@ public class GameOver extends JPanel {
 		this.add(box2);
 		this.add(Box.createVerticalGlue());
 		this.add(box3);
+		this.add(Box.createVerticalGlue());
+		this.add(box4);
+		this.add(Box.createVerticalGlue());
+		this.add(box5);
 		this.add(Box.createVerticalGlue());
 		this.add(Box.createVerticalGlue());
 	}
