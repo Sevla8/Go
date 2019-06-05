d76dd542d4119b624e24bc1031437588c69c830c
Sevla <brandon.da-silva-alves@etu.u-pec.fr>
2019-06-05 23:54:25 +0200

    update Define

--- Define.java
+++ Define.java
@@ -1,13 +1,13 @@
-import java.awt.Dimension;
 import java.awt.Point;
+import java.awt.Dimension;
 
 public class Define {
 	public static Point myFrameLocation = new Point(5, 30);
-	public static Dimension myFramePreferredSize = new Dimension(750, 750);
-	public static Dimension myFrameMinimumSize = new Dimension(400, 400);
-	public static Dimension boardPreferredSize = new Dimension(700, 700);
-	public static Dimension boardMinimumSize = new Dimension(250, 250);
-	public static Dimension infomationPreferredSize = new Dimension(175, 100);
+	public static Dimension myFramePreferredSize = new Dimension(1025, 850);
+	public static Dimension boardPreferredSize = new Dimension(850, 850);
+	public static Dimension infomationPreferredSize = new Dimension(175, 850);
+	public static Dimension myFrameMinimumSize = new Dimension(525, 350);
+	public static Dimension boardMinimumSize = new Dimension(350, 350);
 	public static Dimension informationMinimumSize = new Dimension(175, 350);
 	public static int hoshiPrct = 3;
 }
