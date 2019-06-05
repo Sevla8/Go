02f717adfcf5f890705bafc64dcfd2aa98e2a32f
Sevla <brandon.da-silva-alves@etu.u-pec.fr>
2019-06-05 18:41:59 +0200

    Information

---
+++ Define.java
@@ -0,0 +1,12 @@
+import java.awt.Dimension;
+import java.awt.Point;
+
+public class Define {
+	public static Point myFrameLocation = new Point(5, 30);
+	public static Dimension myFramePreferredSize = new Dimension(750, 750);
+	public static Dimension myFrameMinimumSize = new Dimension(400, 400);
+	public static Dimension boardPreferredSize = new Dimension(700, 700);
+	public static Dimension boardMinimumSize = new Dimension(250, 250);
+	public static Dimension infomationPreferredSize = new Dimension(175, 100);
+	public static Dimension informationMinimumSize = new Dimension(175, 350);
+}
--- Goban.java
+++ Goban.java
@@ -1,92 +1,32 @@
 import java.awt.Color;
-import java.awt.Point;
 import javax.swing.Box;
-import java.awt.Graphics;
+import javax.swing.Timer;
 import javax.swing.JPanel;
-import javax.swing.JLabel;
-import java.awt.Dimension;
-import javax.swing.JButton;
 import javax.swing.BoxLayout;
 
 class Goban extends JPanel {
+	private Timer timer;
 	private Board board;
-	private JLabel turn;
-	private JLabel watch;
-	private JButton skip;
-	private JButton undo;
-	private JButton redo;
-	private JButton giveUp;
-	private JLabel gameOver;
-	private JLabel blackSkip;
-	private JLabel whiteSkip;
-	private JButton getWinner;
-	private JLabel blackPrisoner;
-	private JLabel whitePrisoner;
+	private Information information;
 
 	public Goban(Control control, Go go) {
-		this.setBackground(Color.ORANGE);
+		this.setBackground(Color.BLACK);
+
+		// this.timer = new Timer(go.getParameter().getDelay(), control);
 		this.board = new Board(control, go);
-		this.watch = new JLabel("time : 00:00");
-		this.blackPrisoner = new JLabel("Black Prisoners : 0");
-		this.whitePrisoner = new JLabel("White Prisoners : 0");
-		this.gameOver = new JLabel("Game : In Progress");
-		this.blackSkip = new JLabel("Black Skip : None");
-		this.whiteSkip = new JLabel("White Skip : None");
-		this.turn = new JLabel("Turn : "+this.board.getGo().getTurn());
-		this.undo = new JButton("Undo");
-		this.undo.setEnabled(false);
-		this.redo = new JButton("Redo");
-		this.redo.setEnabled(false);
-		this.skip = new JButton("Skip");
-		this.giveUp = new JButton("Give up");
-		this.getWinner = new JButton("Get Winner");
-		this.getWinner.setEnabled(false);
-		this.undo.addActionListener(control);
-		this.redo.addActionListener(control);
-		this.skip.addActionListener(control);
-		this.giveUp.addActionListener(control);
-		this.getWinner.addActionListener(control);
+		this.information = new Information(control, this.board.getGo().getTurn());
 
-		Box box1 = Box.createVerticalBox();
-		box1.add(Box.createVerticalGlue());
-		box1.add(Box.createVerticalGlue());
-		box1.add(this.gameOver);
-		box1.add(Box.createVerticalGlue());
-		box1.add(this.watch);
-		box1.add(Box.createVerticalGlue());
-		box1.add(this.turn);
-		box1.add(Box.createVerticalGlue());
-		box1.add(this.blackSkip);
-		box1.add(Box.createVerticalGlue());
-		box1.add(this.whiteSkip);
-		box1.add(Box.createVerticalGlue());
-		box1.add(this.blackPrisoner);
-		box1.add(Box.createVerticalGlue());
-		box1.add(this.whitePrisoner);
-		box1.add(Box.createVerticalGlue());
-		box1.add(this.undo);
-		box1.add(Box.createVerticalGlue());
-		box1.add(this.redo);
-		box1.add(Box.createVerticalGlue());
-		box1.add(this.skip);
-		box1.add(Box.createVerticalGlue());
-		box1.add(this.getWinner);
-		box1.add(Box.createVerticalGlue());
-		box1.add(this.giveUp);
-		box1.add(Box.createVerticalGlue());
-		box1.add(Box.createVerticalGlue());
-
-		Box box2 = Box.createVerticalBox();
-		box2.add(Box.createVerticalGlue());
-		box2.add(this.board);
-		box2.add(Box.createVerticalGlue());
+		// Box box = Box.createVerticalBox();
+		// box.add(Box.createVerticalGlue());
+		// box.add(this.board);
+		// box.add(Box.createVerticalGlue());
 
 		this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
 		this.add(Box.createHorizontalGlue());
 		this.add(Box.createHorizontalGlue());
-		this.add(box2);
+		this.add(this.board);
 		this.add(Box.createHorizontalGlue());
-		this.add(box1);
+		this.add(this.information);
 		this.add(Box.createHorizontalGlue());
 		this.add(Box.createHorizontalGlue());
 	}
@@ -94,40 +34,10 @@ class Goban extends JPanel {
 	public Board getBoard() {
 		return this.board;
 	}
-	public void setWatch(JLabel watch) {
-		this.watch = watch;
+	public Information getInformation() {
+		return this.information;
 	}
-	public JButton getSkip() {
-		return this.skip;
-	}
-	public JButton getGiveUp() {
-		return this.giveUp;
-	}
-	public JButton getUndo() {
-		return this.undo;
-	}
-	public JButton getRedo() {
-		return this.redo;
-	}
-	public JLabel getBlackPrisoner() {
-		return this.blackPrisoner;
-	}
-	public JLabel getWhitePrisoner() {
-		return this.whitePrisoner;
-	}
-	public JLabel getGameOver() {
-		return this.gameOver;
-	}
-	public JLabel getBlackSkip() {
-		return this.blackSkip;
-	}
-	public JLabel getWhiteSkip() {
-		return this.whiteSkip;
-	}
-	public JLabel getTurn() {
-		return this.turn;
-	}
-	public JButton getGetWinner() {
-		return this.getWinner;
+	public Timer getTimer() {
+		return this.timer;
 	}
 }
